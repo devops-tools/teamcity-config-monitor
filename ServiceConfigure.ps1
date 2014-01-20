58f8fb6e46467fa6c9c1f33fc71c8e101d119f03
Rob Thijssen <rthijssen@gmail.com>
2014-01-20 14:55:12 +0000

    Added a poke script for the app.config file.

---
+++ ServiceConfigure.ps1
@@ -0,0 +1,24 @@
+param(
+  [Parameter(Mandatory = $true)]
+  [string] $config,
+
+  [string] $teamCityDataDir = $null,
+
+  [Parameter(Mandatory = $true)]
+  [string] $gitConfigName,
+
+  [Parameter(Mandatory = $true)]
+  [string] $gitConfigEmail,
+
+  [string] $gitRemoteRepository = $null
+)
+$xml = (Get-Content $config) -as [xml]
+$xml.SelectSingleNode('/configuration/appSettings/add[@key="GitConfigName"]/@value').set_InnerXML($gitConfigName)
+$xml.SelectSingleNode('/configuration/appSettings/add[@key="GitConfigEmail"]/@value').set_InnerXML($gitConfigEmail)
+if($teamCityDataDir) {
+    $xml.SelectSingleNode('/configuration/appSettings/add[@key="TeamCityDataDir"]/@value').set_InnerXML($teamCityDataDir)
+}
+if($gitRemoteRepository) {
+    $xml.SelectSingleNode('/configuration/appSettings/add[@key="GitRemoteRepository"]/@value').set_InnerXML($gitRemoteRepository)
+}
+$xml.Save($config)
